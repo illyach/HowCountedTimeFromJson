849a8d8f509125261faea7d635223e8e4e515214
illyach <defusejoin@gmail.com>
2022-08-02 20:44:15 +0300

    first commit

---
+++ history.js
@@ -0,0 +1,248 @@
+ let myHistory = [
+    {
+      "Date": "19.07.2022, 22:46",
+      "Seconds": 11330,
+      "Comment": "понял useState page 133 сравнивал два кода Понял родителей компонентов"
+    },
+    {
+      "Date": "18.07.2022, 22:28",
+      "Seconds": 5810,
+      "Comment": "Сделал рейтинг свойств на реакт"
+    },
+    {
+      "Date": "16.07.2022, 19:58",
+      "Seconds": 3662,
+      "Comment": "4 часа page 123 Состояние и хук useState Не смог отобразить иконку FaStar"
+    },
+    {
+      "Date": "16.07.2022, 18:06",
+      "Seconds": 8855,
+      "Comment": ""
+    },
+    {
+      "Date": "15.07.2022, 22:48",
+      "Seconds": 10821,
+      "Comment": "Узнал о деструктуризации props , передача свойст и данных компоненту , webpack понимание и установка"
+    },
+    {
+      "Date": "14.07.2022, 19:55",
+      "Seconds": 4884,
+      "Comment": "20 min Не час"
+    },
+    {
+      "Date": "13.07.2022, 23:07",
+      "Seconds": 5289,
+      "Comment": ""
+    },
+    {
+      "Date": "12.07.2022, 22:13",
+      "Seconds": 5703,
+      "Comment": ""
+    },
+    {
+      "Date": "12.07.2022, 10:59",
+      "Seconds": 3947,
+      "Comment": "11 july"
+    },
+    {
+      "Date": "10.07.2022, 22:02",
+      "Seconds": 8368,
+      "Comment": ""
+    },
+    {
+      "Date": "09.07.2022, 21:56",
+      "Seconds": 2399,
+      "Comment": "4 pages = 60 page"
+    },
+    {
+      "Date": "09.07.2022, 21:06",
+      "Seconds": 6654,
+      "Comment": "2 hour = 8 pages (56 p)"
+    },
+    {
+      "Date": "09.07.2022, 15:48",
+      "Seconds": 2592,
+      "Comment": "1 hour book = 8 pages"
+    },
+    {
+      "Date": "08.07.2022, 18:58",
+      "Seconds": 7499,
+      "Comment": "Good js : 40 pages of react book 2022 O’Reilly "
+    },
+    {
+      "Date": "08.07.2022, 15:30",
+      "Seconds": 182,
+      "Comment": ""
+    },
+    {
+      "Date": "07.07.2022, 22:43",
+      "Seconds": 4292,
+      "Comment": ""
+    },
+    {
+      "Date": "04.07.2022, 22:50",
+      "Seconds": 5521,
+      "Comment": ""
+    },
+    {
+      "Date": "03.07.2022, 22:38",
+      "Seconds": 209,
+      "Comment": ""
+    },
+    {
+      "Date": "03.07.2022, 22:34",
+      "Seconds": 1788,
+      "Comment": ""
+    },
+    {
+      "Date": "03.07.2022, 00:29",
+      "Seconds": 1209,
+      "Comment": ""
+    },
+    {
+      "Date": "02.07.2022, 17:42",
+      "Seconds": 7200,
+      "Comment": "сбиваюсь не понимаю "
+    },
+    {
+      "Date": "01.07.2022, 19:29",
+      "Seconds": 7202,
+      "Comment": "jsx fundamental in Babel"
+    },
+    {
+      "Date": "29.06.2022, 23:10",
+      "Seconds": 765,
+      "Comment": ""
+    },
+    {
+      "Date": "29.06.2022, 17:04",
+      "Seconds": 6320,
+      "Comment": " 2 hours jsx, props, state, React"
+    },
+    {
+      "Date": "28.06.2022, 21:11",
+      "Seconds": 6836,
+      "Comment": ""
+    },
+    {
+      "Date": "28.06.2022, 17:43",
+      "Seconds": 7436,
+      "Comment": ""
+    },
+    {
+      "Date": "27.06.2022, 20:43",
+      "Seconds": 7232,
+      "Comment": "запуск и ебатня с реактом"
+    },
+    {
+      "Date": "27.06.2022, 14:01",
+      "Seconds": 7398,
+      "Comment": "git & github 27 июня —> 2.5 статья"
+    },
+    {
+      "Date": "26.06.2022, 23:19",
+      "Seconds": 7202,
+      "Comment": ""
+    },
+    {
+      "Date": "25.06.2022, 23:30",
+      "Seconds": 3301,
+      "Comment": ""
+    },
+    {
+      "Date": "25.06.2022, 14:44",
+      "Seconds": 2552,
+      "Comment": ""
+    },
+    {
+      "Date": "24.06.2022, 21:51",
+      "Seconds": 6064,
+      "Comment": ""
+    },
+    {
+      "Date": "23.06.2022, 23:58",
+      "Seconds": 4474,
+      "Comment": ""
+    },
+    {
+      "Date": "22.06.2022, 16:07",
+      "Seconds": 834,
+      "Comment": ""
+    },
+    {
+      "Date": "17.06.2022, 22:55",
+      "Seconds": 7733,
+      "Comment": ""
+    },
+    {
+      "Date": "17.06.2022, 00:12",
+      "Seconds": 3508,
+      "Comment": ""
+    },
+    {
+      "Date": "15.06.2022, 22:28",
+      "Seconds": 3003,
+      "Comment": ""
+    },
+    {
+      "Date": "12.06.2022, 22:46",
+      "Seconds": 5168,
+      "Comment": ""
+    },
+    {
+      "Date": "10.06.2022, 23:57",
+      "Seconds": 13376,
+      "Comment": "spread ,\nreast, замыкание, промисы, Отличие присваиваний Примитивов и Обьектов , рекурсивные обходы , Function Expression, Синтаксис new Function"
+    },
+    {
+      "Date": "10.06.2022, 00:27",
+      "Seconds": 1517,
+      "Comment": ""
+    },
+    {
+      "Date": "09.06.2022, 17:04",
+      "Seconds": 2424,
+      "Comment": "stoped wit"
+    },
+    {
+      "Date": "08.06.2022, 20:06",
+      "Seconds": 10817,
+      "Comment": ""
+    },
+    {
+      "Date": "07.06.2022, 18:15",
+      "Seconds": 6673,
+      "Comment": "7 june js"
+    },
+    {
+      "Date": "07.06.2022, 00:14",
+      "Seconds": 4784,
+      "Comment": "6 june js\n"
+    },
+    {
+      "Date": "05.06.2022, 23:25",
+      "Seconds": 7347,
+      "Comment": "js functions\n"
+    },
+    {
+      "Date": "05.06.2022, 13:14",
+      "Seconds": 11,
+      "Comment": ""
+    }
+   ]
+
+
+
+let sum = 0;
+
+let result = myHistory.forEach((value) => {
+  
+        sum += value.Seconds;
+                              // console.log(value.Seconds);
+});
+
+let minutesResult = sum/60;
+let hoursResult = minutesResult/60;
+console.log(sum);
+console.log(minutesResult);
+console.log(hoursResult);
